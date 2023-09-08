0722adde44792185d0cb3f3a8d072faf4a5df912
dfamiliarstranger <eniolaemmanuel@gmail.com>
2023-09-08 18:48:15 +0100

    initial commit

---
+++ api/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils import timezone
+# Create your views here.
+
+github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"  # Replace with your GitHub URL
+github_repo_url = "https://github.com/username/repo"  # Replace with your GitHub URL
+
+def get_info(request):
+    slack_name = request.GET.get('slack_name')
+    track = request.GET.get('track')
+
+    if not slack_name or not track:
+        return JsonResponse({"error": "slack_name and track parameters are required"}, status=400)
+
+    current_day = timezone.now().strftime("%A")
+    utc_time = timezone.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": "200"
+    }
+
+    return JsonResponse(response_data)
