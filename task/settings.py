ca769f0a2b7e78446d929d9684a28f543b768735
dfamiliarstranger <eniolaemmanuel@gmail.com>
2023-09-17 15:13:00 +0100

    database

--- task/settings.py
+++ task/settings.py
@@ -66,14 +66,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'task.wsgi.application'
 
 
-# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-#  'default': dj_database_url.parse(config('DATABASES'))
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
