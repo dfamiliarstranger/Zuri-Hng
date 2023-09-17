7fd59069931f50c44129cb23d02b67e547a98a7a
dfamiliarstranger <eniolaemmanuel@gmail.com>
2023-09-17 13:56:11 +0100

    corrections

--- person/models.py
+++ person/models.py
@@ -1,7 +1,16 @@
+# from django.db import models
+
+# class Person(models.Model):
+#     name = models.CharField(max_length=100,unique=True)
+#     # first_name = models.CharField(max_length=100,unique=True)
+#     # last_name = models.CharField(max_length=100, unique=True)
+#     # age = models.PositiveIntegerField()
+   
 from django.db import models
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=100,unique=True)
-    last_name = models.CharField(max_length=100, unique=True)
-    age = models.PositiveIntegerField()
-   
+    first_name = models.CharField(max_length=255, unique=True)  # Example field definition
+    # Other fi
+
+    def __str__(self):
+        return self.first_name
--- person/views.py
+++ person/views.py
@@ -1,39 +1,39 @@
-from rest_framework import generics
-from rest_framework import status
-from rest_framework.response import Response
-from .models import Person
-from .serializers import PersonSerializer
-from django.db.models import Q
-from django.http import Http404
+# from rest_framework import generics
+# from rest_framework import status
+# from rest_framework.response import Response
+# from .models import Person
+# from .serializers import PersonSerializer
+# from django.db.models import Q
+# from django.http import Http404
 
-class PersonCreateView(generics.CreateAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+# class PersonCreateView(generics.CreateAPIView):
+#     queryset = Person.objects.all()
+#     serializer_class = PersonSerializer
 
-class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = PersonSerializer
-    lookup_field = 'identifier'  # Define the lookup field to match the URL parameter
+# class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = PersonSerializer
+#     lookup_field = 'identifier'  # Define the lookup field to match the URL parameter
 
-    def get_object(self):
-        identifier = self.kwargs.get('identifier')
+#     def get_object(self):
+#         identifier = self.kwargs.get('identifier')
 
-        # Check if the identifier is a valid integer (user ID)
-        if identifier.isdigit():
-            return Person.objects.get(pk=identifier)
-        else:
-            # If it's not an integer, assume it's a name and try to retrieve by name
-            person = Person.objects.filter(Q(first_name__iexact=identifier) | Q(last_name__iexact=identifier)).first()
-            if not person:
-                raise Http404('Person not found')
-            return person
+#         # Check if the identifier is a valid integer (user ID)
+#         if identifier.isdigit():
+#             return Person.objects.get(pk=identifier)
+#         else:
+#             # If it's not an integer, assume it's a name and try to retrieve by name
+#             person = Person.objects.filter(Q(name__iexact=identifier) | Q(last_name__iexact=identifier)).first()
+#             if not person:
+#                 raise Http404('Person not found')
+#             return person
 
-    def put(self, request, *args, **kwargs):
-        # Implement the put method to update the person's details
-        return self.partial_update(request, *args, **kwargs)
+#     def put(self, request, *args, **kwargs):
+#         # Implement the put method to update the person's details
+#         return self.partial_update(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
-        # Implement the delete method to remove the person
-        return self.destroy(request, *args, **kwargs)
+#     def delete(self, request, *args, **kwargs):
+#         # Implement the delete method to remove the person
+#         return self.destroy(request, *args, **kwargs)
 
 
     # def get_queryset(self):
@@ -59,3 +59,41 @@ class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
+from django.db.models import Q
+from django.http import Http404
+
+
+class PersonCreateView(generics.CreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PersonSerializer
+    lookup_field = 'identifier'  # Define the lookup field to match the URL parameter
+
+    def get_object(self):
+        identifier = self.kwargs.get('identifier')
+
+        # Check if the identifier is a valid integer (user ID)
+        if identifier.isdigit():
+            return Person.objects.get(pk=identifier)
+        else:
+            # If it's not an integer, assume it's a name and try to retrieve by name
+            # Correct query using the 'name' field
+            person = Person.objects.filter(Q(first_name__iexact=identifier)).first()
+
+            if not person:
+                raise Http404('Person not found')
+            return person
+
+    def put(self, request, *args, **kwargs):
+        # Implement the put method to update the person's details
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        # Implement the delete method to remove the person
+        return self.destroy(request, *args, **kwargs)
